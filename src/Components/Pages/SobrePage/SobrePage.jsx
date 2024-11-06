2931a5c5af185c7499bea77c1c1dabf191249ccf
Icaro Jordano <Icarojordano>
2024-11-06 00:44:45 -0300

    feat: Rota Sobre version mobile

--- src/Components/Pages/SobrePage/SobrePage.jsx
+++ src/Components/Pages/SobrePage/SobrePage.jsx
@@ -1,7 +1,13 @@
 export function SobrePage() {
     return(
-        <div className="h-screen bg-red-500">
-            aaa
+        <div className="h-screen pt-20">
+            <h2 className="text-xl  p-4 text-white">Hello, I'm Icaro Jordano<span className="text-teal-400">.</span></h2>
+            
+            <h3 className="text-2xl p-4 text-white">Desenvolvedor web <span className="text-teal-400">Front-end</span> Atuando em Fortaleza, Ceará</h3>
+
+            <p className="text-white p-4">Comecei minha jornada na programação há 4 anos, atualmente me especializo em criar e implementar designs de interfaces web voltadas para o Frontend, utilizando ferramentas de desenvolvimento como <span className="text-teal-400">HTML, CSS, JavaScript, React, Bootstrap e Tailwind</span>. Meu objetivo é sempre aprimorar minhas habilidades e contribuir para projetos que façam a diferença no dia a dia dos usuários</p>
+
+            <p className="text-white p-4">Ao longo dos anos, tive a oportunidade de trabalhar em projetos que exigiram não apenas habilidades técnicas, mas também uma compreensão profunda das necessidades do usuário. Estou sempre em busca de novos desafios que me permitam expandir meus conhecimentos e habilidades. Gosto de colaborar em equipe e ja tem pratica com alguns metodos de <span className="text-teal-400" >versionamento de codigo.</span></p>
         </div>
     )
 }
