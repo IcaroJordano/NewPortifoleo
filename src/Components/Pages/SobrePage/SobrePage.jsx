c67cced180b641d4a43f42b351cea19ce9c61c3f
Icaro Jordano <Icarojordano>
2024-11-06 01:05:58 -0300

    feat(Sobre): ajuste dark mode

--- src/Components/Pages/SobrePage/SobrePage.jsx
+++ src/Components/Pages/SobrePage/SobrePage.jsx
@@ -1,14 +1,14 @@
 export function SobrePage() {
     return (
         <div className=" pt-20">
-            <h2 className="text-xl lg:text-3xl  lg:mx-10 p-4 text-white">Hello, I'm Icaro Jordano<span className="text-teal-400">.</span></h2>
+            <h2 className="text-xl lg:text-3xl  lg:mx-10 p-4 text-white lg:text-neutral-700">Hello, I'm Icaro Jordano<span className="text-teal-400">.</span></h2>
 
-            <h3 className="text-2xl lg:text-4xl lg:mx-10 p-4 text-white">Desenvolvedor web <span className="text-teal-400">Front-end</span> Atuando em Fortaleza, Ceará</h3>
+            <h3 className="text-2xl lg:text-4xl lg:mx-10 p-4 text-white lg:text-neutral-700">Desenvolvedor web <span className="text-teal-400">Front-end</span> Atuando em Fortaleza, Ceará</h3>
 
             <div className="flex items-center justify-around lg:my-10" >
-                <div className="lg:w-7/12  text-neutral-300/80" >
-                    <p className=" p-4 lg:text-lg leading-7 lg:leading-8 ">Comecei minha jornada na programação há 4 anos, atualmente me especializo em criar e implementar designs de interfaces web voltadas para o Frontend, utilizando ferramentas de desenvolvimento como <span className="text-teal-400">HTML, CSS, JavaScript, React, Bootstrap e Tailwind</span>. Meu objetivo é sempre aprimorar minhas habilidades e contribuir para projetos que façam a diferença no dia a dia dos usuários</p>
-                    <p className=" p-4 lg:text-lg leading-7 lg:leading-8 ">Ao longo dos anos, tive a oportunidade de trabalhar em projetos que exigiram não apenas habilidades técnicas, mas também uma compreensão profunda das necessidades do usuário. Estou sempre em busca de novos desafios que me permitam expandir meus conhecimentos e habilidades. Gosto de colaborar em equipe e ja tem pratica com alguns metodos de <span className="text-teal-400" >versionamento de codigo.</span></p>
+                <div className="lg:w-7/12  text-neutral-100/90 dark:text-neutral-600" >
+                    <p className=" p-4 lg:text-base leading-7 lg:leading-8 ">Comecei minha jornada na programação há 4 anos, atualmente me especializo em criar e implementar designs de interfaces web voltadas para o Frontend, utilizando ferramentas de desenvolvimento como <span className="text-teal-400">HTML, CSS, JavaScript, React, Bootstrap e Tailwind</span>. Meu objetivo é sempre aprimorar minhas habilidades e contribuir para projetos que façam a diferença no dia a dia dos usuários</p>
+                    <p className=" p-4 lg:text-base leading-7 lg:leading-8 ">Ao longo dos anos, tive a oportunidade de trabalhar em projetos que exigiram não apenas habilidades técnicas, mas também uma compreensão profunda das necessidades do usuário. Estou sempre em busca de novos desafios que me permitam expandir meus conhecimentos e habilidades. Gosto de colaborar em equipe e ja tem pratica com alguns metodos de <span className="text-teal-400" >versionamento de codigo.</span></p>
                 </div>
                 <img className="w-80 h-80 rounded-2xl hidden lg:block" src="https://icarojordano.github.io/Portifoleo/assets/avatar-ChLfIcQ1.jpeg" alt="" />
             </div>
