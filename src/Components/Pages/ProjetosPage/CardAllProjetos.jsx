7dba0cec681ac93757c6683a650c62947c40fd1d
Icaro Jordano <Icarojordano>
2024-11-06 05:11:35 -0300

    feat: config props of CardAllProjetos

--- src/Components/Pages/ProjetosPage/CardAllProjetos.jsx
+++ src/Components/Pages/ProjetosPage/CardAllProjetos.jsx
@@ -3,26 +3,28 @@ import { FiExternalLink } from "react-icons/fi";
 
 import { FaGithub } from "react-icons/fa";
 
-export function CardAllProjetos(params) {
-    return(
+export function CardAllProjetos({ imagem, titulo, github, deploy }) {
+    return (
         <div className="w-80 lg:w-96 h-56 lg:h-64 mb-10 lg:ml-6 ">
-        <div className="h-5/6 rounded-t-md justify-center flex relative  overflow-hidden">
-            <img className="w-full  object-cover" src="https://icarojordano.github.io/Conference-Schedule/assets/Infomation-BabZBlIZ.avif" alt="" />
-            <h3 className="absolute text-xl text-white top-32 lg:top-40 left-5">E-shop Building in React</h3>
-        </div>
-        <div className="h-1/6 rounded-b-md items-center justify-around bg-neutral-200/90 flex">
-            <h4 className="text-base text-nowrap hidden line-clamp-1 " >E-sssssshaaop</h4>
-            <div className="text-xs h-full  justify-center flex items-center text-neutral-500">
-                <span className="px-1 hover:underline" >React</span>
-                <span className="px-1 hover:underline" >Tailwind</span>
-                <span className="px-1 hover:underline  " >Bootstrap</span>
-                <span className="px-1 hover:underline hidden lg:block" >Responsivo</span>
+            <div className="h-5/6 rounded-t-md justify-center flex relative  overflow-hidden">
+                <a className="justify-center w-full flex" href={deploy} target="_blank" >
+                    <img className="w-full  object-cover opacity-80" src={imagem} alt="" />
+                </a>
+                <h3 className="absolute text-xl text-white top-32 lg:top-40 left-5">{titulo}</h3>
             </div>
-            <div className="flex text-neutral-900 text-xl items-center h-full ml-14">
-                <FaGithub       className=" flex mx-1  "></FaGithub>
-                <FiExternalLink className=" flex mx-1  "></FiExternalLink>
+            <div className="h-1/6 rounded-b-md items-center justify-around bg-neutral-200/90 flex">
+                <div className="text-xs h-full  justify-center flex items-center text-neutral-500">
+                    <span className="px-1 hover:underline" >React</span>
+                    <span className="px-1 hover:underline" >Tailwind</span>
+                    <span className="px-1 hover:underline  " >Bootstrap</span>
+                    <span className="px-1 hover:underline hidden lg:block" >Responsivo</span>
+                </div>
+                <div className="flex text-neutral-900 text-xl items-center h-full ml-14">
+                    <a target="_blank" href={github}><FaGithub className=" flex mx-1  "></FaGithub></a>
+                    <a target="_blank" href={deploy}><FiExternalLink className=" flex mx-1  "></FiExternalLink></a>
+
+                </div>
             </div>
         </div>
-    </div>
     )
 }
--- src/Components/Pages/ProjetosPage/ProjetosPage.jsx
+++ src/Components/Pages/ProjetosPage/ProjetosPage.jsx
@@ -4,11 +4,11 @@ export function ProjetosPage() {
     return (
         <div className="  flex justify-center pt-28">
             <div className=" flex mx-auto justify-around lg:justify-normal flex-wrap ">
-                <CardAllProjetos></CardAllProjetos>
-                <CardAllProjetos></CardAllProjetos>
-                <CardAllProjetos></CardAllProjetos>
-                <CardAllProjetos></CardAllProjetos>
-                <CardAllProjetos></CardAllProjetos>
+                <CardAllProjetos deploy={'https://icarojordano.github.io/Tailwind/'} imagem={'https://spotlight.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&w=384&q=75'} titulo={"Portifoleo Tailwind"}></CardAllProjetos>
+                <CardAllProjetos deploy={'https://icarojordano.github.io/Conference-Schedule/'} imagem={'https://icarojordano.github.io/Conference-Schedule/assets/Infomation-BabZBlIZ.avif'} titulo={"Anuncio de Evento"}></CardAllProjetos>
+                <CardAllProjetos deploy={'https://icarojordano.github.io/React-e-commerc/'} imagem={'https://i.imgur.com/cSytoSD.jpeg,https://i.imgur.com/WwKucXb.jpeg,https://i.imgur.com/cE2Dxh9.jpeg'} titulo={"E-Shop Building React"}></CardAllProjetos>
+                <CardAllProjetos deploy={'https://icarojordano.github.io/1-Marketing-website/'} imagem={'https://icarojordano.github.io/1-Marketing-website/assets/fonebranco-DcaGvqC0.png'} titulo={'One Page Product'}></CardAllProjetos>
+                <CardAllProjetos deploy={'https://icarojordano.github.io/Portifoleo/'} imagem={'https://icarojordano.github.io/Portifoleo/assets/avatar-ChLfIcQ1.jpeg'} titulo={'My First Portfoleo'}></CardAllProjetos>
             </div>
         </div>
     )
