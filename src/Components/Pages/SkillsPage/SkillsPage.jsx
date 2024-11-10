4a6b7c9bc177f2240bde303c96e20baab47b7c44
Icaro Jordano <Icarojordano>
2024-11-10 05:02:12 -0300

    fix: corrige tamanho da div SkillsPage

--- src/Components/Pages/SkillsPage/SkillsPage.jsx
+++ src/Components/Pages/SkillsPage/SkillsPage.jsx
@@ -4,7 +4,7 @@ import { SoftSkills } from "./SoftSkills";
 
 export function SkillsPage(params) {
     return (
-        <div className=" pt-20 w-screen lg:w-11/12">
+        <div className=" pt-20   min-w-full lg:w-11/12">
             <SoftSkills></SoftSkills>
             <Metodologias></Metodologias>
             <HardSkills></HardSkills>
--- src/Components/Pages/SkillsPage/SoftSkills.jsx
+++ src/Components/Pages/SkillsPage/SoftSkills.jsx
@@ -5,7 +5,7 @@ import { DivSoftSkills } from "./DivSoftSkills";
 
 export function SoftSkills() {
     return (
-        <div className=" flex flex-col">
+        <div className=" flex flex-col  w-full">
             <h2 className=" ml-4 lg:ml-8 w-max  underline  rounded-2xl h-10 items-center text-neutral-400 dark:text-neutral-600  flex" ><FaFeatherAlt className="text-white dark:text-black mx-2"></FaFeatherAlt> Soft Skills</h2>
             <div>
                 <SlideSoftSkills></SlideSoftSkills>
